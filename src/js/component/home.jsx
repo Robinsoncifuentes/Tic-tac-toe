445d3986debc0ae544890970b5fe2a29aa4b00dd
Robinsoncifuentes <robinsonmunoz4@gmail.com>
2022-02-09 21:07:51 +0000

    en desarrollo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,15 @@
 import React from "react";
+import { Formulario } from "./formulario.jsx";
+import { Board } from "./Board.jsx";
+import { Square } from "./square.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<h1 className="header">Tic Tac Toe in React.js</h1>
+
+			<Board />
+		</>
 	);
 };
 
---
+++ src/js/component/square.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export const Square = (props) => {
+	return (
+		<div>
+			<button className="btn" onClick={() => props.onClick()}>
+				{props.value}
+			</button>
+		</div>
+	);
+};
